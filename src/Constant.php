1fa6c6b70f4303ef1990321549ec7c23d063910e
Imdvlpr99 <imdvlpr.agency@gmail.com>
2023-12-10 20:52:12 +0700

    add function for category

--- src/Constant.php
+++ src/Constant.php
@@ -11,14 +11,25 @@ define('DATABASE', 'imdvlprm_daily');
  * TABLE NAME
  */
 define('TODO_LIST', 'todo_list');
+define('CATEGORY_LIST', 'category_list');
 
 
 /**
  * TABLE ROW
  */
+
+ /**
+  * TODO_LIST TABLE
+  */
 define('ID', 'id');
 define('TITLE', 'title');
 define('DESCRIPTION', 'description');
 define('DATE', 'date');
 define('TIME', 'time');
+define('CATEGORY_ID', 'category_id');
 define('IS_COMPLETED', 'is_complete');
+
+ /**
+  * CATEGORY_LIST TABLE
+  */
+define('CATEGORY_NAME', 'category_name');
