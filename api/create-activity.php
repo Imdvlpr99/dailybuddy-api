cd2327e738dee3ffb102a03e6e2503dbf83066a3
Imdvlpr99 <imdvlpr.agency@gmail.com>
2023-12-13 18:21:36 +0700

    fix : add parameter for create activity

--- api/create-activity.php
+++ api/create-activity.php
@@ -5,4 +5,4 @@ require('../src/Api.php');
 use Imdvlpr\Api;
 
 $app = new Api();
-echo $app->addList($_POST['title'], $_POST['desc'], $_POST['is_complete']);
+echo $app->addList($_POST['title'], $_POST['desc'], $_POST['date'], $_POST['time'], $_POST['category_id'], $_POST['is_complete']);
